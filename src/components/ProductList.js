e8221e9335b4d8af0cfb9fc079e8c54228be5723
VishalSaini7272 <sainivishal156@gmail.com>
2025-11-06 17:47:46 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,1 +1,32 @@
 
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ProductList = ({ products }) => {
+  return (
+    <div className="container">
+      <h2>Available Mobiles</h2>
+      <div className="row">
+        {products.map((product) => (
+          <div className="col-12" key={product.id}>
+            <div>
+              <Link to={`/products/${product.id}`}>
+                <div className="row">
+                  <div className="col-md-3">
+                    <img src={product.image} alt={product.name} width="150" />
+                  </div>
+                  <div className="col-md-9">
+                    <h3>{product.name}</h3>
+                    <p>${product.price}</p>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
